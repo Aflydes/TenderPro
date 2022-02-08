bc7ce5e5ef121179683e18d300c01829a5ec0c9d
Aflydes <aflydes@gmail.com>
2022-02-08 12:57:19 +0300

    fix

--- js/sliders.js
+++ js/sliders.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
-    
+    $('.reviews__slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        variableWidth: true,
+        appendArrows: '.reviews__slider-arrows',
+        prevArrow: '<svg class="slider-arrow slider-arrow__prev" height="40" width="40"><use xlink:href="img/icons.svg#arrow"></use></svg>',
+        nextArrow: '<svg class="slider-arrow slider-arrow__next"  height="40" width="40"><use xlink:href="img/icons.svg#arrow"></use></svg>', 
+        dots: true
+      });
 });
